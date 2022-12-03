1cff06ace278603e9bfd3247fc8d46e2b31e4018
macnguyennguyenhuan <macnguyennguyenhuan@gmail.com>
2022-12-03 17:38:28 +0700

    unfinished

--- js/script.js
+++ js/script.js
@@ -1,64 +1,135 @@
 
 //Contructor Validator
 function Validator(options){
+
+    function getParent(element,selector){
+        while(element.parentElement){
+            if(element.parentElement.matches(selector)){
+                return element.parentElement;
+            }
+            element = element.parentElement;
+        }
+    }
+
+
     var selectorRules = {};
     function Validate(inputElement, rule){
+        //var errorElement = getParent(inputElement, '.form-item')
         var errorMessage;
-        var errorElement = inputElement.parentElement.querySelector(options.errorMessage)            
+        var errorElement = getParent(inputElement,options.formGroupSelector).querySelector(options.errorMessage)            
         
-        //Selector's rule
         var rules = selectorRules[rule.selector];
-        
-        
-        for(var i = 0; i < rules.length; i++){
-           errorMessage = rules[i](inputElement.value)
+        for(var i = 0 ; i < rules.length ; ++i){
+            switch(inputElement.type){
+                case 'radio':
+
+                    case 'checkbox':
+                        errorMessage = rules[i](
+                            formElement.querySelector(rule.selector + ':checked'));
+                        break;
+                        default:
+                            errorMessage = rules[i](inputElement.value);
+            }
+           
            if(errorMessage) break;
         }
 
+
         if(errorMessage){
             errorElement.innerText = errorMessage;
-            inputElement.parentElement.classList.add('invalid')
+            getParent(inputElement,options.formGroupSelector).classList.add('invalid')
         }
         else{
             errorElement.innerText = '';
-            inputElement.parentElement.classList.remove('invalid')
+            getParent(inputElement,options.formGroupSelector).classList.remove('invalid')
+        }
+        return !errorMessage;
+    }
+    
+    var formElement = document.querySelector(options.form);
+    if(formElement){
+        //submit form
+        formElement.onsubmit = function(e){
+            e.preventDefault();
+            var isFormValid = true;
+
+        //validate when click submit without fill form
+            options.rules.forEach(function(rule){
+                var inputElement = formElement.querySelector(rule.selector)
+                var isValid = Validate(inputElement,rule)
+                if(!isValid)
+                {
+                    isFormValid = false;
+                }
+
+                
+            });
+            
+
+            if(isFormValid){
+                if(typeof options.onSubmit === 'function'){
+                    var enableInputs = formElement.querySelectorAll('[name]');
+                    var formValues = Array.from(enableInputs).reduce(function(values,input){
+                       switch(input.type){
+                           case 'radio':
+                            case 'checkbox':
+                                if(input.matches(':checked')){
+
+                                }
+                                
+                                break;
+                                default:
+                                
+                       }
+                        
+                        return  values;
+                    },{});
+                    options.onSubmit(formValues);
+                } 
+                else{
+                    formElement.submit();
+                } 
+            }
+          
         }
         
     }
-   
-    var formElement = document.querySelector(options.form)
     
     if(formElement){
         options.rules.forEach(function(rule){
             //Save rule for each selector
+             
+            
             if(Array.isArray(selectorRules[rule.selector])){
-
+                selectorRules[rule.selector].push(rule.test);
             }
             else{
-                selectorRules[rule.selector] = rule.test;
+                selectorRules[rule.selector] = [rule.test]
             }
-            
-            var inputElement = formElement.querySelector(rule.selector)
-            if(inputElement){
+
+            var inputElements = formElement.querySelectorAll(rule.selector)
+
+            Array.from(inputElements).forEach(function(inputElement){
                 inputElement.onblur = function(){
                     Validate(inputElement,rule)
                 }
-            }
-            inputElement.oninput = function(){
-                var errorElement = inputElement.parentElement.querySelector(options.errorMessage)            
-                errorElement.innerText = '';
-                inputElement.parentElement.classList.remove('invalid')
-                }
             
+            inputElement.oninput = function(){
+                var errorElement = getParent(inputElement,options.formGroupSelector).querySelector(options.errorMessage)            
+                errorElement.innerText = '';
+                getParent(inputElement,options.formGroupSelector).classList.remove('invalid')
+                }
+            });
         });
     }
+    
 }
 //Define Rules
 Validator.isRequired = function(selector,message){
     return{
         selector:selector,
         test: function(value){
-            return value.trim() ? undefined : message ||  'Please fill this form'
+            return value ? undefined : message ||  'Please fill this form'
         }
     }
 }
